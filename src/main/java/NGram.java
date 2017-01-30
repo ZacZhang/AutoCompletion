e812ed61988c5e1ae731db84875295b413c64dd4
zaczhang19 <zaczhang19@gmail.com>
2017-01-30 13:48:59 -0500

    mapper and reducer function finished

--- src/main/java/NGram.java
+++ src/main/java/NGram.java
@@ -16,21 +16,53 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class NGram {
 
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+    public static class NGramMapper extends Mapper<Object, Text, Text, IntWritable> {
 
+        int numOfGram;
+        //setup方法只在初始化的时候被调用一次
+        @Override
+        public void setup(Context context) {
+            Configuration conf = context.getConfiguration();
+            numOfGram = conf.getInt("numOfGram", 5);//这个project的numOfGram是从命令行读入
+        }
 
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            /*
+            input: read sentence
+            I love data n=3
+            I love -> 1
+            love data -> 1
+            I love data -> 1
+            */
 
-
+            String line = value.toString().trim().toLowerCase().replaceAll("[^a-z]", " ");//非字母的都会被替换成空格
+            String[] words = line.split("\\s+");// "\\s"表示空格,回车,换行等空白符,"+"号表示一个或多个的意思
+            if (words.length < 2) {
+                return;
+            }
+            StringBuilder sb;
+            for (int i = 1; i < words.length; i++) {
+                sb = new StringBuilder();
+                sb.append(words[i]);
+                for (int j = 1; i + j < words.length && j < numOfGram; j++) {
+                    sb.append(" ");
+                    sb.append(words[i + j]);
+                    context.write(new Text(sb.toString()), new IntWritable(1));
+                }
+            }
         }
     }
 
-    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public static class NGramReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
         @Override
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-
+            int sum = 0;
+            for (IntWritable value: values) {
+                sum = sum + value.get();
+            }
+            context.write(key, new IntWritable(sum));
         }
     }
 
